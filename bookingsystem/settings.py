5d879f88cf6ca25e966abc9fb6747e5684bb21cc
Edward Printer <59025133+E-Printer@users.noreply.github.com>
2024-12-03 11:59:05 +0000

    Update settings.py

--- bookingsystem/settings.py
+++ bookingsystem/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-el%j0^uim*ip&zko3ko16y4xe)ci$@uf1#m&n9v86r$9iuipxe'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
